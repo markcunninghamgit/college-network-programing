faf3974d7f6cbc2afae5cad7995984f8d1f1ce02
Mark Cunningham <markcunninghamemail@gmail.com>
2012-03-07 16:35:31 +0000

    changes to tcp client vars etc (not done)

--- tcp/client-tcp.c
+++ tcp/client-tcp.c
@@ -20,9 +20,10 @@ int main( argc, argv )
 {
 	char *host;  				/* will hold the host name */ 
 	char *service; 			/* will hold the port number */ 
-	char buf[ LINELEN+1 ];		/* buffer for one line of text	*/
+	char lineBuffer[ LINELEN+1 ];		/* buffer for one line of text	*/
 	char bufin [ LINELEN+1 ] ;		/* buffer for reading line of test */
-	int s, n;				/* socket descriptor, read count*/
+	int socketConnection;				/* socket descriptor, read count*/
+	int n;
 	int outchars=	LINELEN, inchars=LINELEN;      /* max characters sent & received	*/ 
 
 	if (argc != 3 )
@@ -31,10 +32,13 @@ int main( argc, argv )
 		exit( 1 );
 	}
 	else
+	{
 		host = argv[1];
+	}
+
 	service = argv[2];
-	/*connectsock creates and connects a socket to specified host & port no. */
-	s = connectsock ( host, service );
+	/*createSocketConnection creates and connects a socket to specified host & port no. */
+	socketConnection = createSocketConnection ( host, service );
 	/* keep reading characters from the keyboard
 	   write them out to the server via the socket, read reply from server
 	   and print out the returned string  until fgets returns a value 0
@@ -48,45 +52,47 @@ int main( argc, argv )
 		/* read it back  - read returns with n holding the no. of bytes read, or 0 for end-of-file or negative for error*/
 		/* place socket read statement here to read data into bufin  use n to hold the nujmber of characters read*/
 		bufin[ n ] = '\0';  		/* ensure bufin has a string terminator null */
+
 		if ( n < 0 )
 		{
 			fprintf( stderr, "socket read failed: %s\n", strerror( errno));
 			exit( 1 );
 		}
+
 		fputs( bufin, stdout );
 	}
 }
 
 /*------------------------------------------------------------------------
- * connectsock - allocate & connect a socket using TCP 
+ * createSocketConnection - allocate & connect a socket using TCP 
  *------------------------------------------------------------------------
  */
-int connectsock( 
+int createSocketConnection( 
 		char *host,		/* name of host to which connection is desired	*/
 		char *service	/*  port number as a character string	*/
 	       )	
 
 {
-	struct hostent *phe;		/* pointer to data structure containing IP address as 4-byte integer*/
-	struct sockaddr_in sin;	/* will hold a full Internet endpoint address*/
+	struct hostent *serverHostDetails;		/* pointer to data structure containing IP address as 4-byte integer*/
+	struct sockaddr_in serverAddress;	/* will hold a full Internet endpoint address*/
 	/* full address comprises address family, port number and IP address */
-	int s ;			/* socket descriptor */
-	bzero( (char *)&sin, sizeof( sin ) );  /* initialise sin with zeros */
-	sin.sin_family = AF_INET;     /* place address family in sin.sin_family */
+	int socketConnection ;			/* socket descriptor */
+	bzero( (char *)&serverAddrss, sizeof( serverAddress ) );  /* initialise sin with zeros */
+	serverAddress.sin_family = AF_INET;     /* place address family in sin.sin_family */
 
 	/* function atoi converts port number from character string to integer*/
 	/* htons converts from host to network byte order*/
 	/* place resultant port number into sin.sin_port */
-	if ( ( sin.sin_port = htons( (ushort)atoi( service ) ) ) == 0 )
+	if ( ( serverAddress.sin_port = htons( (ushort)atoi( service ) ) ) == 0 )
 	{
 		fprintf( stderr, "can't get \"%s\" service entry\n", service );
 		exit( 1 );
 	}
 
-	/* gethostbyname converts host name to 4-byte IP address and places it in phe->h_addr */
+	/* gethostbyname converts host name to 4-byte IP address and places it in serverHostDetails->h_addr */
 	/* bcopy copies 4-byte host IP address into sin.sin_addr */
-	if ( phe = gethostbyname( host ) )
-		bcopy( phe->h_addr, &sin.sin_addr,4 );
+	if ( serverHostDetails = gethostbyname( host ) )
+		bcopy( serverHostDetails->h_addr, &serverAddress.sin_addr,4 );
 	else 
 	{
 		fprintf( stderr, "can't get \"%s\" host entry\n", host );
@@ -98,7 +104,7 @@ int connectsock(
 	/* SOCK_STREAM is a symbolic constant = 1 */ 
 	/* AF_INET is a symbolic constant = 2 indicating address family TCP/IP */
 	/* Put socket function call in here. Let s = socket descriptor */
-	if ( s < 0 )
+	if ( socketConnection < 0 )
 	{
 		fprintf( stderr, "can't create socket: %s\n", strerror( errno ) );
 		exit( 1 );
@@ -113,5 +119,5 @@ int connectsock(
 		exit( 1 );
 	}
 
-	return s;
+	return socketConnection;
 }
